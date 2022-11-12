2e1038168c47281ebf75cee110c59b408dd34db7
Yazan Hasan <y.hasan-e@arada.com>
2022-11-12 13:30:27 +0400

    add report charts

--- src/screens/Report.tsx
+++ src/screens/Report.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { BarChart, PieChart } from "react-native-chart-kit";
 import { data, dataType } from "../statics/data";
-
 const Report = () => {
   const [reportData, setReportData] = useState<dataType[]>([]);
   const [age13_18, setAge13_18] = useState<number>(0);
   const [age19_25, setAge19_25] = useState<number>(0);
   const [age25_, setAge25] = useState<number>(0);
   const [guestNumber, setGuestNumber] = useState<number>(0);
+  const [hostNumber, setHostNumber] = useState<number>(0);
   const [student, setStudent] = useState<string | number>("");
   const [localites, setLocalites] = useState<{ [key: string]: any }>({});
-
   const [profession, setProfession] = useState<string | number>("");
   useEffect(() => {
     calculateReportData();
@@ -26,6 +26,7 @@ const Report = () => {
     let pr = 0;
     let count = {};
     let GuestCount = 0;
+    var HostCount = 0;
     reportData?.map((element: any) => {
       if (element.age <= 18 && element.age >= 13) {
         a = a + 1;
@@ -41,6 +42,8 @@ const Report = () => {
       }
       if (element.guest) {
         GuestCount = GuestCount + 1;
+      } else {
+        HostCount = HostCount + 1;
       }
       if (element.localities in count) {
         count[element.localities] = count[element.localities] + 1;
@@ -48,6 +51,7 @@ const Report = () => {
         count[element.localities] = 1;
       }
       setGuestNumber(GuestCount);
+      setHostNumber(HostCount);
       setLocalites(count);
     });
 
@@ -57,58 +61,137 @@ const Report = () => {
     setStudent(st);
     setProfession(pr);
   };
-
+  const chartConfig = {
+    backgroundGradientFrom: "grey",
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    decimalPlaces: 0,
+  };
+  const AgeData = [
+    {
+      name: "in 13-18 age range",
+      number: age13_18,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "in 8-25 age range ",
+      number: age19_25,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "in 25+ age range ",
+      number: age25_,
+      color: "#ffa726",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+  ];
+  const GuestsData = [
+    {
+      name: "Guest",
+      number: guestNumber,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Host",
+      number: hostNumber,
+      color: "#ffa726",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+  ];
+  const ProfessionData = [
+    {
+      name: "Profession",
+      number: profession,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Students",
+      number: student,
+      color: "#ffa726",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+  ];
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.titleContainer1}>
         <Text style={styles.title}>Number of people in a given age range</Text>
       </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.dataView}>13-18 : </Text>
-        <Text style={styles.dataText}>{age13_18} </Text>
-        <Text style={styles.dataView}>18-25 : </Text>
-        <Text style={styles.dataText}>{age19_25} </Text>
-        <Text style={styles.dataView}>25+ : </Text>
-        <Text style={styles.dataText}>{age25_} </Text>
-      </View>
+      <PieChart
+        data={AgeData}
+        width={400}
+        height={220}
+        chartConfig={chartConfig}
+        accessor="number"
+        backgroundColor="transparent"
+        paddingLeft="0"
+        absolute
+      />
+
       <View style={styles.titleContainer1}>
         <Text style={styles.title}>Number of people by localities</Text>
       </View>
-      <ScrollView
-        style={{ height: "25%" }}
-        showsVerticalScrollIndicator={false}
-      >
-        {Object.entries(localites).map(([key, value], i) => {
-          return (
-            <View key={i} style={[styles.titleContainer, { borderWidth: 1 }]}>
-              <Text>{key}</Text>
-              <Text>{value}</Text>
-            </View>
-          );
-        })}
+      <ScrollView horizontal>
+        <BarChart
+          data={{
+            labels: Object?.keys(localites),
+            datasets: [
+              {
+                data: Object?.values(localites),
+              },
+            ],
+          }}
+          width={2900}
+          height={250}
+          chartConfig={chartConfig}
+          verticalLabelRotation={10}
+          yAxisLabel={""}
+          yAxisSuffix={""}
+        />
       </ScrollView>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>
           Average group size of people attending the event
         </Text>
       </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.dataView}>Guests : </Text>
-        <Text style={styles.dataText}>{guestNumber}</Text>
-      </View>
+      <PieChart
+        data={GuestsData}
+        width={400}
+        height={220}
+        chartConfig={chartConfig}
+        accessor="number"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute
+      />
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Average Professionals & students count</Text>
       </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.dataView}>Profession : </Text>
-        <Text style={styles.dataText}>{profession}</Text>
-        <Text style={(styles.dataView, { marginStart: 10 })}>Student : </Text>
-        <Text style={styles.dataText}>{student} </Text>
-      </View>
-    </View>
+      <PieChart
+        data={ProfessionData}
+        width={400}
+        height={220}
+        chartConfig={chartConfig}
+        accessor="number"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute
+      />
+    </ScrollView>
   );
 };
 export default Report;
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
